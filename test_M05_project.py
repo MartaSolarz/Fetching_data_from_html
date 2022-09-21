07d50f5a10acc162a2ee7de9b1f2b00cb2a81b2e
Marta Solarz <102785427+MartaSolarz@users.noreply.github.com>
2022-09-21 12:18:43 +0200

    Update test_M05_project.py

--- test_M05_project.py
+++ test_M05_project.py
@@ -1,40 +1,16 @@
-from M05_project import text_formating
-from M05_project import extract
+from processing_text import TextFormating
+from featching_manager import FeatchingData
 
 
 def test_remove_punctation():
-
-    """Removing punctuation marks from text"""
-
     text = 'New ?! example'
-    got = text_formating(text)
+    got = TextFormating(text).remove_special_chars()
     expected = 'New    example'
     assert got == expected
 
 
 def test_remove_special_chars():
     text = 'New\texample\n'
-    got = text_formating(text)
+    got = TextFormating(text).remove_tabs_and_enters()
     expected = 'New example '
-    assert got == expected
-
-
-def test_remove_whitespaces():
-
-    """Removing whitespace at the beginning and end of a string"""
-
-    text = '<b> Example </b>'
-    xpath = '//b'
-    got = extract(text, xpath)
-    expected = ['Example']
-    assert got == expected
-
-
-def test_scrapper_reallife_website():
-    with open('M05\Materiały Budowlane - Znajdź Materiały Budowlane w Leroy Merlin.html', encoding='utf8') as stream:
-        content = stream.read()
-    
-    xpath = "//*[@id='product-categories']/div[1]/div[1]/ul/li/a"
-    got = extract(content, xpath)
-    expected = ['Cegły klinkierowe', 'Cegły  pustaki i bloczki betonowe', 'Cementy i Wapna', 'Piasek  keramzyt']
     assert got == expected
